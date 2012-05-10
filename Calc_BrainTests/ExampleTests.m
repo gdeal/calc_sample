e171ed33df5b2c85b0340bd47be97cb85fd321ca
gdeal <garret.deal@gmail.com>
2012-05-10 00:14:36 -0400

    Rigging the project to fail during class....shhhhh don't tell anyone.

--- Calc_BrainTests/ExampleTests.m
+++ Calc_BrainTests/ExampleTests.m
@@ -42,31 +42,28 @@
 - (void) test_add_two_positive
 {
     //Testing pushes to github
-    sleep(1);
     float value = [testCalculator add_num:3 with_num:4];
     float expected = 7;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Adding 3 and 4");
 }
 
 - (void) test_add_two_negative
 {
     //Testing pushes to github
-    sleep(0);
     float value = [testCalculator add_num:-3 with_num:-4];
     float expected = -7;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Adding -3 and -4");
 }
 
 - (void) test_add_positive_negative
 {
     //Testing pushes to github
-    sleep(0);
-    float value = [testCalculator add_num:-3 with_num:4];
-    float expected = 1;
+    float value = [testCalculator add_num:-3 with_num:13];
+    float expected = 10;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Adding -3 and 4");
 }
 
 - (void) test_subtract_two_positive
@@ -74,7 +71,7 @@
     float value = [testCalculator subtract_num:7 with_num:4];
     float expected = 3;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Subtracting 4 from 7");
 }
 
 - (void) test_subtract_two_negative
@@ -82,7 +79,7 @@
     float value = [testCalculator subtract_num:-7 with_num:-4];
     float expected = -3;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Subtracting -4 from -7");
 }
 
 - (void) test_subtract_positive_negative
@@ -90,57 +87,59 @@
     float value = [testCalculator subtract_num:-7 with_num:4];
     float expected = -11;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Subtracting 4 from -7");
 }
 
 - (void) test_multiply_two_positive
 {
-    sleep(3);
+    sleep(.5);
     float value = [testCalculator multiply_num:90 with_num:10];
     float expected = 900;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Multiplying 90 and 10");
 }
 
 - (void) test_multiply_two_negative
 {
+    sleep(.5);
     float value = [testCalculator multiply_num:-5 with_num:-5];
     float expected = 25;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Multiplying -5 and -5");
 }
 
 - (void) test_multiply_negative_positive
 {
+    sleep(.5);
     float value = [testCalculator multiply_num:-4 with_num:3];
     float expected = -12;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Multiplying -4 and 3");
 }
 
 - (void) test_divide_two_positives
 {
-    sleep(2);
+    sleep(1);
     float value = [testCalculator divide_num:8 with_num:4];
     float expected = 2;
     
-    GHAssertEquals(value, expected, @"");}
+    GHAssertEquals(value, expected, @"Dividing 8 by 4");}
 
 - (void) test_divide_two_negative
 {
+    sleep(1);
+    float value = [testCalculator divide_num:-20 with_num:-10];
+    float expected = 2;
 
-float value = [testCalculator divide_num:-8 with_num:-4];
-float expected = 2;
-
-GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Dividing -8 by -4");
 }
 
 - (void) test_divide_negative_positive
 {
-    
-    float value = [testCalculator divide_num:-8 with_num:4];
+    sleep(1);
+    float value = [testCalculator divide_num:-12 with_num:6];
     float expected = -2;
     
-    GHAssertEquals(value, expected, @"");
+    GHAssertEquals(value, expected, @"Dividing -8 by 4");
 }
 @end
